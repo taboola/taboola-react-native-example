9e2fd2c6fe55bd9f8f0e302923a5a0db6e098fa9
mark.sauer.utley <mark.sauer.utley@taboola.com>
2019-06-28 11:56:13 -0400

    update widget and feed page

--- full-example/TaboolaReactNativeExample/components/ArticleWithWidgetAndFeed.js
+++ full-example/TaboolaReactNativeExample/components/ArticleWithWidgetAndFeed.js
@@ -22,8 +22,37 @@ class ArticleWithWidgetAndFeed extends React.Component {
 			<View>
 				<BackButton onPress={e => this.props.back()} title="Back" />
 				<ScrollView>
-					<Header>Here's a Taboola Widget</Header>
-
+					<Header>Here's a Taboola Widget and Feed</Header>
+					<StyledText>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						quae ab illo inventore veritatis et quasi architecto beatae vitae
+						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+						eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+						qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+						sed quia non numquam eius modi tempora incidunt ut labore et dolore
+						magnam aliquam quaerat voluptatem.
+					</StyledText>
+					<StyledText>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						quae ab illo inventore veritatis et quasi architecto beatae vitae
+						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+						eos qui ratione voluptatem sequi nesciunt.
+					</StyledText>
+					<StyledText>
+						Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+						accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+						quae ab illo inventore veritatis et quasi architecto beatae vitae
+						dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+						aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
+						eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est,
+						qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit,
+						sed quia non numquam eius modi tempora incidunt ut labore et dolore
+						magnam aliquam quaerat voluptatem.
+					</StyledText>
 					<Widget
 						viewID={this.state.viewID}
 						onDidLoad={e => this.setWidgetLoaded()}
