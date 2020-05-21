2c3254e6fada156990616ab46c11de25b8db8a3a
Mark Sauer-Utley <mark.sauer.utley@taboola.com>
2020-05-21 12:07:22 -0400

    add platform-specific publisher account example
    android bundle update

--- full-example/TaboolaReactNativeExample/components/Widget.js
+++ full-example/TaboolaReactNativeExample/components/Widget.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
+import { Platform } from 'react-native';
 import RNTaboolaView from '@taboola/react-native-taboola';
 import PropTypes from 'prop-types';
 
-const Widget = props => {
+const Widget = (props) => {
+	// Set the publisher according to the OS. 
+	// Taboola will provide a publisher id for android and one for ios.
+	// Here we are just using the test account for both.
+	const publisher = Platform.OS === 'ios' ? 'sdk-tester' : 'sdk-tester';
 	const [height, setHeight] = useState(0);
 
 	return (
 		<RNTaboolaView
 			mode={props.mode}
-			publisher={props.publisher}
+			publisher={publisher}
 			pageType={props.pageType}
 			pageUrl={props.pageUrl}
 			placement={props.placement}
@@ -23,7 +28,7 @@ const Widget = props => {
 				// This lets us implement other logic into this callback via props
 				props.onDidLoad(event);
 				// Set the height of the widget dynamically
-				setHeight(parseInt(event.nativeEvent.height, 10));
+				setHeight(Number(event.nativeEvent.height));
 			}}
 			onDidFailToLoad={event => {
 				props.onDidFailToLoad();
@@ -40,7 +45,6 @@ const Widget = props => {
 
 Widget.defaultProps = {
 	mode: 'alternating-widget-without-video-1-on-1',
-	publisher: 'sdk-tester',
 	pageType: 'article',
 	pageUrl: 'https://blog.taboola.com',
 	placement: 'Mid Article',
@@ -50,7 +54,6 @@ Widget.defaultProps = {
 
 Widget.propTypes = {
 	mode: PropTypes.string.isRequired,
-	publisher: PropTypes.string.isRequired,
 	pageType: PropTypes.string.isRequired,
 	pageUrl: PropTypes.string.isRequired,
 	placement: PropTypes.string.isRequired,
