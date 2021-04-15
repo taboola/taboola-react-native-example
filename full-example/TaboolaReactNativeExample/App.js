e43f43f0429a22c1f8c1bc53015dc487d0720d34
Rotem Tal <rotem.t@taboola.com>
2021-04-15 19:00:10 +0300

    Version text font adjustment

--- full-example/TaboolaReactNativeExample/App.js
+++ full-example/TaboolaReactNativeExample/App.js
@@ -60,9 +60,9 @@ class App extends React.Component {
         return (
             <Container>
                 <ScrollView>
-                    <Text style={{paddingTop:15, textAlign: 'center', fontSize: 10 }}>Taboola SDK-2.9.0 iOS / 2.8.3 Android;  </Text>
-                    <Text style={{paddingTop:10, textAlign: 'center', fontSize: 10 }}> Taboola React Native Plugin  - 2.2.0; </Text>
-                    <Text style={{paddingTop:10, textAlign: 'center', fontSize: 10 }}>React Native version 0.59.10; </Text>
+                    <Text style={{paddingTop:50, textAlign: 'center', fontSize: 15 }}>Taboola React Native Plugin  - 2.2.0; </Text>
+                    <Text style={{paddingTop:15, textAlign: 'center', fontSize: 15 }}>Taboola SDK-2.9.0 iOS / 2.8.3 Android;  </Text>
+                    <Text style={{paddingTop:15, textAlign: 'center', fontSize: 15 }}>React Native version 0.59.10; </Text>
                     {this.renderSwitch()}
                 </ScrollView>
 
